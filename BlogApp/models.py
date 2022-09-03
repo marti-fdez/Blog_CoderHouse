4ac995a1477d80bba90e1c669acf9be017a85dab
Marti Fdez <60945840+marti-fdez@users.noreply.github.com>
2022-09-03 21:10:48 +0200

    Primer commit con la base de datos formada

---
+++ BlogApp/models.py
@@ -0,0 +1,23 @@
+
+from django.db import models
+
+# Create your models here.
+class Profesion(models.Model):
+    nombre = models.CharField(max_length=100)
+    sueldo = models.IntegerField()
+
+
+
+class Blogger(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    profesion = models.ForeignKey('Profesion', on_delete = models.CASCADE)
+    telefono = models.CharField(max_length=9)
+    email = models.EmailField()
+
+
+class Articulo(models.Model):
+    autor = models.ForeignKey('Blogger', on_delete = models.CASCADE)
+    fecha_publicacion = models.DateField()
+    tematica = models.CharField(max_length=250)
+    cantidad_paginas = models.IntegerField()
