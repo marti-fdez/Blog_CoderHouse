733cba230927f442070473540f743c54de336a6f
Marti Fernandez <fmarti001@gmail.com>
2022-09-06 19:26:39 +0200

    url-inicial

--- CodeProject/urls.py
+++ CodeProject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog-app-index/', include('BlogApp.urls')),
+    path('blog-python/', include('BlogApp.urls')),
 ]
