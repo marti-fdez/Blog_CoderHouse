9c9bca30765b59d71f22c3cf72c4233f1236d61f
Mariano Cuartero <cuarteroma@gmail.com>
2022-10-03 10:09:01 -0300

    Correcciones finales 1

--- Perfiles/models.py
+++ Perfiles/models.py
@@ -8,4 +8,4 @@ class Avatar(models.Model):
     imagen = models.ImageField(upload_to="avatares", null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     def __str__(self):
-        return f"Avatar de {self.user.username}"
+        return f"Avatar de {self.user}"
